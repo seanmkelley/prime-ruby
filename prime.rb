aa26d861b196c405ef94f8c09aaed060f2732440
seankelley <smkelley.1997@gmail.com>
2020-12-18 16:21:26 -0500

    commit tree 1c9c0453c423b1c1d1a4048c8fc406b31a087947

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+    if num.abs < 2
+        return false 
+    end 
+    i = 2
+    while i <= Math.sqrt(num.abs)
+        if num % i == 0
+            return false 
+        end
+        i += 1
+    end 
+    return true 
+end
+ 
